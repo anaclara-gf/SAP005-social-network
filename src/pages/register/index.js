151f12f0d1c07737800eaf5f2a8685869f829ec1
gabriella-guimaraes <gabigara13@gmail.com>
2020-12-20 16:20:52 -0300

    responsive settings

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,9 +1,40 @@
+import { SignUp } from "../../services/index.js";
+
 export const Register = () => {
-    // Coloque sua página
-    const rootElement = document.createElement('div');
-    rootElement.innerHTML = `
-        <h1>Olá, mundo!</h1>
+  const rootElement = document.createElement('div');
+  rootElement.innerHTML = `
+        <div class="flex-container">
+            <img class="flex-itens logo-image" src="../images/should-I-watch--logo.jpg">
+            <section class="registerText">
+                <h1>Register: join our community!</h1>
+            </section>
+            <form class="flex-container">
+                <label class="flex-itens" for="name">Name:</label>
+                <input class="flex-itens" id="name" type="text" placeholder="" required>
+                <label class="flex-itens" for="username">Username:</label>
+                <input class="flex-itens" id="username" type="text" placeholder="" required>
+                <label class="flex-itens" for="email">E-mail:</label>
+                <input class="flex-itens" id="email" type="email" placeholder="E-mail" required>
+                <label class="flex-itens" for="password">Password:</label>
+                <input class="flex-itens" id="password" type="password" placeholder="Password" required>
+                <button class="flex-itens" id="signup-button">Sign up</button>
+                <button class="flex-itens">Sign up with Google</button>
+            </form>
+            <p class="flex-itens">Already have an account?</p>
+            <button class="flex-itens">Sign in</button>
+        </div>
     `;
+
+    let name = rootElement.querySelector("#name");
+    let username = rootElement.querySelector("#username");
+    let email = rootElement.querySelector("#email");
+    let password = rootElement.querySelector("#password");
+    let signUpButton = rootElement.querySelector("#signup-button");
+
+    signUpButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        SignUp(email.value, password.value, name.value, username.value);
+    })
+
     return rootElement;
-  };
-  
+};
---
+++ src/services/firebaseConfig.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: 'AIzaSyA2t1S0fAMMcmMKlrgvySHIETfxDgwsTC4',
+  authDomain: 'rede-social-329c5.firebaseapp.com',
+  projectId: 'rede-social-329c5',
+  storageBucket: 'rede-social-329c5.appspot.com',
+  messagingSenderId: '380478275045',
+  appId: '1:380478275045:web:236a15ee4af6c4392ac30c',
+  measurementId: 'G-LQNR916SGW'
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+firebase.firestore();
