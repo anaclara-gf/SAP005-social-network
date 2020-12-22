35c57e83ecca0d65ba758d6ba00acc27eb73c91c
LauraDeperon <lauradeperonribeiro@gmail.com>
2020-12-22 13:36:40 -0300

    redefinição de rotas

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import { Home } from './pages/home/index.js';
 import { Login } from './pages/login/index.js';
-import { Register } from './pages/register/auth.js';
-import { Profile } from './pages/register/profile.js';
+import { Register } from './pages/register/index.js';
+import { Profile } from './pages/profile/index.js';
 import { onNavigate } from './utils/history.js';
 
 const routeRender = () => {
@@ -24,19 +24,25 @@ window.addEventListener('load', () => {
     .getElementById('home')
     .addEventListener('click', (e) => {
       e.preventDefault();
-      onNavigate('/')
+      onNavigate('/home')
     });
   document
     .getElementById('login')
     .addEventListener('click', (e) => {
       e.preventDefault();
-      onNavigate('/login')
+      onNavigate('/')
     });
   document
     .getElementById('register')
     .addEventListener('click', (e) => {
       e.preventDefault();
       onNavigate('/register')
+    });
+    document
+    .getElementById('profile')
+    .addEventListener('click', (e) => {
+      e.preventDefault();
+      onNavigate('/profile')
     });
   routeRender();
   ;
