bf3bfece4c588bef0680482e7d9493f80b205ad7
gabriella-guimaraes <gabigara13@gmail.com>
2020-12-22 15:25:58 -0300

    email verification function adapted and responsive

--- src/services/index.js
+++ src/services/index.js
@@ -1,49 +1,13 @@
 import { onNavigate } from "../utils/history.js";
 
-// export const SignUp = (email, password, name, username) => firebase.auth().createUserWithEmailAndPassword(email, password)
-//   .then((res) => {
-    // const user = firebase.auth().currentUser;
-    // user.sendEmailVerification().then(() => {
-    //   alert('Email sent. Please check your inbox.');
-    //   // Email sent.
-    // }).catch((error) => {
-    //   alert('Ooops, something went wrong!');
-    //   const errorCode = error.code;
-    //   const errorMessage = error.message;
-    //   console.log(errorCode);
-    //   console.log(errorMessage);
-    //   // An error happened.
-    // });
-//     // user.sendEmailVerification()
-//     //   .then(() => {
-//     //     if (!firebase.auth().currentUser.emailVerified) {
-//     //       return alert('Invalid email');
-//     //     }
-//     //     alert('Email sent! Please check your inbox');
-//     //   })
-//     //   .catch((error) => {
-//     //     alert('Ooops, something went wrong!');
-//     //     const errorCode = error.code;
-//     //     const errorMessage = error.message;
-//     //     console.log(errorCode);
-//     //     console.log(errorMessage);
-//     //   });
-//     return user.updateProfile({
-//       displayName: name,
-//       uid: username,
-//     });
-//   })
-//   .catch((error) => {
-//     alert('Ooops, something went wrong!');
-//     const errorCode = error.code;
-//     const errorMessage = error.message;
-//     console.log(errorCode);
-//     console.log(errorMessage);
-//   });
-
 export const SignUp = (email, password) => {
   return firebase.auth().createUserWithEmailAndPassword(email, password)
     .then(() => {
+        const user =firebase.auth().currentUser;
+        user.sendEmailVerification()
+        .then(() => {
+            alert('Email sent. Please check your inbox.')
+        });
         alert("Congrats! Now, tell us about you!");
         onNavigate("/profile");
     })
