596ffb39855903c2ea3441af1d4d5b8c44ff3364
LauraDeperon <lauradeperonribeiro@gmail.com>
2020-12-22 16:33:22 -0300

    salvar mudanças

--- src/services/index.js
+++ src/services/index.js
@@ -1,16 +1,21 @@
 import { onNavigate } from "../utils/history.js";
 
 export const SignUp = (email, password) => {
-  return firebase.auth().createUserWithEmailAndPassword(email, password)
-    .then(() => {
-        alert("Congrats! Now, tell us about you!");
-        onNavigate("/profile");
-    })
-    .catch((error) => {
-        alert("Ooops, something went wrong!")
-        console.log(error.code);
-        console.log(error.message);
-    })
+    return firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then(() => {
+            const user = String(firebase.auth().currentUser.uid);
+            firebase.firestore().collection('users').doc(user).set({
+                email: user.email,
+                uid: user.uid
+            })
+            alert("Congrats! Now, tell us about you!");
+            onNavigate("/profile");
+        })
+        .catch((error) => {
+            alert("Ooops, something went wrong!")
+            console.log(error.code);
+            console.log(error.message);
+        })
 };
 
 export const InfoProfile = (name, username, bio, favGenres) => {
@@ -21,53 +26,68 @@ export const InfoProfile = (name, username, bio, favGenres) => {
         bio: bio,
         favGenres: favGenres
     })
-    .then(() => {
-        alert("Welcome to 'Should I Watch?'");
-        onNavigate("/home");
-    })
+        .then(() => {
+            alert("Welcome to 'Should I Watch?'");
+            onNavigate("/home");
+        })
 };
 
 export const signIn = (email, password) => {
     return firebase.auth().signInWithEmailAndPassword(email, password)
-    .then(() => {
-        onNavigate("/home")
-    })
-    .catch((error) => {
-        alert("Ooops, something went wrong!")
-        console.log(error.code);
-        console.log(error.message);
-    })
+        .then(() => {
+            onNavigate("/home")
+        })
+        .catch((error) => {
+            alert("Ooops, something went wrong!")
+            console.log(error.code);
+            console.log(error.message);
+        })
 };
 
 export const signOut = (email, password) => {
     return firebase.auth().signOut()
-    .then(() => {
-        onNavigate("/");
-    })
-    .catch((error) => {
-        alert("Ooops, something went wrong!")
-        console.log(error.code);
-        console.log(error.message);
-    })
+        .then(() => {
+            onNavigate("/");
+        })
+        .catch((error) => {
+            alert("Ooops, something went wrong!")
+            console.log(error.code);
+            console.log(error.message);
+        })
 };
 
 export const signInGoogle = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
-    return firebase.auth().signInWithPopup(provider)
-    .then((result) => {
-        if(){
-        onNavigate("/home")
-        /*const token = result.credential.accessToken;
-        const user = result.user;*/
-        }else{
-        onNavigate("/profile")
-        }
-    })
-    .catch((error) => {
-        alert("Ooops, something went wrong!")
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        const email = error.email;
-        const credential = error.credential;
-    })
+    return firebase.auth().signInWithRedirect(provider)
+        .then(() => {
+            oi();
+        })
+        .catch((error) => {
+            alert("Ooops, something went wrong!")
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            const email = error.email;
+            const credential = error.credential;
+        })
 };
+
+export const oi = () => {
+    firebase.firestore().collection('users').where("email", "===", user.email).get()
+        .then((result) => {
+            if (result.size < 1) {
+                const user = String(firebase.auth().currentUser.uid);
+                firebase.firestore().collection('users').doc(user).set({
+                    email: user.email,
+                    uid: user.uid
+                })
+                onNavigate("/profile")
+            } else {
+                onNavigate("/home")
+            }
+        })
+        .catch((error) => {
+            alert("Ooops, something went wrong!")
+            console.log(error.code);
+            console.log(error.message);
+        })
+}
