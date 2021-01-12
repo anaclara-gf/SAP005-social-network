499af87d848ea22d69b21ed35683725382329520
LauraDeperon <lauradeperonribeiro@gmail.com>
2021-01-12 15:24:28 -0300

    adicionando alguns arquivos faltantes

--- src/services/index.js
+++ src/services/index.js
@@ -104,14 +104,3 @@ export const DisagreePostClickOut = (postId) => {
         disagree: firebase.firestore.FieldValue.arrayRemove(UserInfoUid())
     })
 };
-<<<<<<< HEAD
-=======
-
-export const stayLogged = () => {
-   return firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
-}
-export const SearchAgreeClicks = () => {
-    const usersRef = firebase.firestore().collection('reviews');
-    return usersRef.where('agree', "array-contains", UserInfoUid()).get();
-};
->>>>>>> master
