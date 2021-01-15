5ce0b029c04499ece50a6f793b659f17fa1bea99
anaclara-gf <anaclaragf@outlook.com>
2021-01-15 13:59:47 -0300

    bugs

--- src/router.js
+++ src/router.js
@@ -3,7 +3,7 @@ import { Register } from './pages/register/index.js';
 import { Profile } from './pages/profile/index.js';
 import { Timeline } from './pages/timeline/index.js';
 import { onNavigate } from './utils/history.js';
-import { verifyUser, InfoProfileEmail } from './services/index.js';
+import { verifyUser, InfoProfileEmail, UserProfileInfo } from './services/index.js';
 
 const routeRender = () => {
   const rootDiv = document.getElementById('root');
@@ -28,10 +28,19 @@ window.addEventListener('load', () => {
           if (result.size < 1) {
             InfoProfileEmail();
             onNavigate('/profile');
-          } else {
-            onNavigate('/timeline')
           }
         })
+      
+      UserProfileInfo(user.id)
+        .then(doc => {
+          const data = doc.data();
+          if(data.name === null || data.username === null || data.bio === null || data.favGenres === null){
+            onNavigate('/profile');
+          } else {
+            onNavigate('/timeline');
+          }
+        })
+        
     }else{
       onNavigate('/')
     }
